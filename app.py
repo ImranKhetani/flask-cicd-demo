57c37bdbed2354437a80c55b24bb7a932f417001
ImranKhetani <imran_khetani@hotmail.com>
2025-04-07 03:53:20 +0000

    Updated App.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, CI/CD!"
+    return "Hello, CI/CD! Welcome to our App"
 
 if __name__ == '__main__':
     app.run(debug=True)
